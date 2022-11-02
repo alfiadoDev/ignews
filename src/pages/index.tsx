cf1d578bc9df4d6c07cfecff4df61da69a731dd1
Alfiado Constantino <alfiado.ngana@xit2s.com>
2022-11-02 16:02:53 +0200

    server side rendiring SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: string
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,13 +24,31 @@ export default function Home() {
           <h1>News about the <span>React <span>world.</span></span></h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 monthly</span>
+            <span>for {product.amount} monthly</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Avatar" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LzgJtCXxAvllE5ciLrlTIXH')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'usd',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    }
+  }
 }
